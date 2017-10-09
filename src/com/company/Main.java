1cf41de28e1fcd8c67d41f49f28dd3a7a57d4bd6
Joyce Yang <jcyang@hmc.edu>
2017-10-09 11:05:40 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello, World!");
+	System.out.println("My name is Joyce Yang.");
+	System.out.println("Today is October 9, 2017.");
+    }
+}
